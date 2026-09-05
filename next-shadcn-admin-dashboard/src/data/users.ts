b25604b8d16872a81b6ac1f804ac24a75b5a8c3e
Rhae-Shane <omeshkumar9813499778@gmail.com>
2026-09-05 15:28:46 +0530

    feat(app): add seed users data

---
+++ next-shadcn-admin-dashboard/src/data/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    name: "Omesh Kumar",
+    username: "Rhae-Shane",
+    email: "hello@omesh.vercel.app",
+    avatar: "https://github.com/Rhae-Shane.png",
+    role: "administrator",
+  },
+  {
+    id: "2",
+    name: "Operator",
+    username: "operator",
+    email: "ops@omesh.vercel.app",
+    avatar: "",
+    role: "admin",
+  },
+];
+
+export const rootUser = users[0];
